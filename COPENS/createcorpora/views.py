2a44058eeb5451353e884b2bdc98d308ddd4908e
puerdon <don0726@elifeconnection.com>
2018-12-09 22:42:10 +0800

    add mixins in user panel page

--- COPENS/createcorpora/views.py
+++ COPENS/createcorpora/views.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from django.db.models import Q
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
@@ -14,7 +15,7 @@ from django.conf import settings
 from .forms import UploadCorpusForm, SearchForm
 from . import utils
 from .models import Corpus, CopensUser
-
+from .mixins import MultiFormMixin, MultiFormsView
 
 class Home(ListView):
     template_name = 'createcorpora/home.html'
@@ -34,6 +35,8 @@ class SearchView(FormView):
     """
     User chooses what databases to query from.
     """
+    print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
     template_name = 'createcorpora/search.html'
     form_class = SearchForm
 
@@ -57,6 +60,7 @@ class ResultsView(View):
     template_name = 'createcorpora/results.html'
 
     def get(self, request):
+        print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
         if not self.request.GET.get('corpora'):
             messages.warning(self.request, 'At least one database must be selected.')
             return redirect('create:search')
@@ -69,6 +73,7 @@ class ResultsView(View):
             return render(request, self.template_name, {'results': results})
 
         form = SearchForm(self.request.GET)
+        print('xxxxxxxxxxxx')
         if form.is_valid():
             results_list = []
             user_registry = utils.get_user_registry(self.request)
@@ -134,5 +139,63 @@ class UploadCorporaView(LoginRequiredMixin, FormView):
         return super().form_valid(form)
 
 
-class UserPanelView(UploadCorporaView):
+class UserPanelView(LoginRequiredMixin, MultiFormsView):
+    login_url = 'account_login'
     template_name = 'createcorpora/user_panel.html'
+
+    form_classes = {
+        'upload': UploadCorpusForm,
+        'search': SearchForm
+    }
+
+    success_urls = {
+        'upload': reverse_lazy('create:home'),
+        'search': ''
+    }
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        copens_user = CopensUser.objects.get(user=self.request.user)
+        corpora = [c for c in Corpus.objects.filter(Q(owner=copens_user))]
+        context['no_corpus'] = True if len(corpora) == 0 else False
+        return context
+
+    def upload_form_valid(self, form):
+        print(list(form.cleaned_data.items()))
+        file = form.cleaned_data['file']
+        p_attrs = form.cleaned_data['positional_attrs']
+        s_attrs = form.cleaned_data['structural_attrs']
+        zh_name = form.cleaned_data['zh_name']
+        en_name = form.cleaned_data['en_name']
+        is_public = form.cleaned_data['is_public']
+
+        copens_user = get_object_or_404(CopensUser, user=self.request.user)
+        print(copens_user.user)
+        raw_dir = Path(copens_user.raw_dir)
+        data_dir = Path(copens_user.data_dir)
+        registry_dir = Path(copens_user.registry_dir)
+
+        print(raw_dir.joinpath(file.name))
+        if raw_dir.joinpath(file.name).exists():
+            messages.warning(self.request, 'This corpus already exists.')
+            return redirect('create:upload')
+
+        utils.save_file_to_drive(file, raw_dir)
+        utils.cwb_encode(vrt_file=raw_dir / file.name, data_dir=data_dir,
+                         registry_dir=registry_dir, p_attrs=p_attrs, s_attrs=s_attrs)
+        utils.cwb_make(Path(file.name).stem, registry_dir=registry_dir)
+
+        Corpus.objects.create(
+            owner=copens_user,
+            zh_name=zh_name,
+            en_name=en_name,
+            is_public=is_public,
+        )
+        if is_public:
+            os.link(registry_dir.joinpath(file.name.split('.')[0]),
+                    Path(settings.CWB_PUBLIC_REG_DIR).joinpath(file.name.split('.')[0]),
+                    )
+        return super().upload_form_valid(form)
+
+    def search_form_valid(self, form):
+        print(form.cleaned_data.items())
