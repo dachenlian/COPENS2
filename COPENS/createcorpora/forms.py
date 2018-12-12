e8df1e371ce1d3b20aa8c086aac3a4c2ddafd278
richl <cclemon.lian@gmail.com>
2018-12-12 18:21:15 +0800

    rearranged form fields; UploadCorpusForm disables inputs based on preprocessing checkbox

--- COPENS/createcorpora/forms.py
+++ COPENS/createcorpora/forms.py
@@ -16,14 +16,14 @@ class UploadCorpusForm(forms.Form):
     file = forms.FileField(label='檔案', help_text='檔案名稱必須與語料庫名稱(英文)相同')
     needs_preprocessing = forms.BooleanField(required=False, help_text='我們可以幫您做前處理的部份',
                                              label='自動處理')
+    is_public = forms.BooleanField(required=False, help_text="您是否希望公開這份語料庫嗎？",
+                                   label='設為公開')
     positional_attrs = forms.CharField(max_length=255, help_text="Please prepend each attribute with a '-P'.",
                                        required=False, initial='-P pos',
                                        label='位置屬性(Positional attributes)')
     structural_attrs = forms.CharField(max_length=255, help_text="Please prepend each attribute with a '-S'.",
                                        required=False, initial='-S text+id -S s -0 corpus',
                                        label='結構屬性(Structural attributes)')
-    is_public = forms.BooleanField(required=False, help_text="您是否希望公開這份語料庫嗎？",
-                                   label='設為公開')
 
     def clean_positional_attrs(self):
         cleaned_data = self.cleaned_data['positional_attrs']
