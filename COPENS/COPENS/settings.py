d40c99800641031d1c117fac2c3aa79f5fbcac98
dachenlian <cclemon.lian@gmail.com>
2018-12-15 03:50:51 +0800

    removed Google sign in

--- COPENS/COPENS/settings.py
+++ COPENS/COPENS/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
-    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.google',
 
     'copens_static_pages.apps.CopensStaticPagesConfig',
     'createcorpora.apps.CreatecorporaConfig',
